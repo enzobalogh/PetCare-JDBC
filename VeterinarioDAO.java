2d749fad6753b552286c190b76d01f62516fa7c2
enzobalogh <enzobalogh2@gmail.com>
2025-11-23 13:14:21 -0300

    Add files via upload

---
+++ VeterinarioDAO.java
@@ -0,0 +1,88 @@
+package br.com.petcare.dao;
+
+import br.com.petcare.database.ConnectionFactory;
+import br.com.petcare.model.Proprietario;
+import br.com.petcare.model.Veterinario;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class VeterinarioDAO {
+
+    public void inserir(Veterinario v) {
+        String sql = "INSERT INTO Veterinarios (nome, crmv, especialidade, telefone) VALUES (?, ?, ?, ?)";
+        try (Connection conn = ConnectionFactory.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setString(1, v.getNome());
+            stmt.setString(2, v.getCrmv());
+            stmt.setString(3, v.getEspecialidade());
+            stmt.setString(4, v.getTelefone());
+
+            stmt.executeUpdate();
+            System.out.println("Veterinário inserido com sucesso!");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public List<Veterinario> listarTodos() {
+        List<Veterinario> lista = new ArrayList<>();
+        String sql = "SELECT * FROM Veterinarios";
+        try (Connection conn = ConnectionFactory.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql);
+             ResultSet rs = stmt.executeQuery()) {
+
+            while (rs.next()) {
+                Veterinario v = new Veterinario();
+                v.setId(rs.getInt("id_veterinario"));
+                v.setNome(rs.getString("nome"));
+                v.setCrmv(rs.getString("crmv"));
+                v.setEspecialidade(rs.getString("especialidade"));
+                v.setTelefone(rs.getString("telefone"));
+                lista.add(v);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return lista;
+    }
+
+    public void atualizar(Veterinario v){
+        String sql = "UPDATE Veterinario SET nome_veterinario = ?, crmv = ?, especialidade = ?, telefone = ? WHERE id_veterinario = ?";
+
+        try(Connection conn = ConnectionFactory.getConnection();
+            PreparedStatement stmt = conn.prepareStatement(sql)){
+
+            stmt.setString(1, v.getNome());
+            stmt.setString(2, v.getCrmv());
+            stmt.setString(3, v.getEspecialidade());
+            stmt.setString(4, v.getTelefone());
+            stmt.setInt(5, v.getId());
+
+            stmt.executeUpdate();
+        } catch (Exception e){
+            e.printStackTrace();
+            e.getMessage();
+        }
+    }
+
+    public void deletar(int id){
+        String sql = "DELETE FROM Veterinario WHERE id_veterinario = ?";
+
+        try (Connection conn = ConnectionFactory.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)){
+
+            stmt.setInt(1,id);
+            stmt.executeUpdate();
+
+        } catch (Exception e){
+            e.printStackTrace();
+            e.getMessage();
+        }
+
+
+
+    }
+
+}
