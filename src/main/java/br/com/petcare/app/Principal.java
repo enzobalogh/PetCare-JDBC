bf8b1aaecf6315fd68086ad02a18d09b5f07b246
enzobalogh <enzobalogh2@gmail.com>
2025-11-23 15:10:58 -0300

    Adicionando classe Principal

--- src/main/java/br/com/petcare/app/Principal.java
+++ src/main/java/br/com/petcare/app/Principal.java
@@ -16,6 +16,7 @@ public class Principal extends Menu {
         ProprietarioDAO proprietarioDAO = new ProprietarioDAO();
         Scanner sc = new Scanner(System.in);
         int opcao;
+        int opcao2;
 
         do {
             System.out.println("===== PETCARE =====");
@@ -28,48 +29,138 @@ public class Principal extends Menu {
             opcao = sc.nextInt();
             sc.nextLine();
 
-            switch(opcao) {
-
+            switch (opcao) {
                 case 1:
-                    Proprietario p = lerDadosProprietario();
-                    proprietarioDAO.inserir(p);
-                    System.out.println("\n== LISTA DE PROPRIETÁRIOS ==");
-                    List<Proprietario> proprietarios = proprietarioDAO.listarTodos();
-                    for(Proprietario dono : proprietarios){
-                        System.out.println(
-                                "ID: "+dono.getId() +
-                                        ", Nome: "+dono.getNome() +
-                                        ", CPF: "+dono.getCpf() +
-                                        ", Telefone: "+dono.getTelefone() +
-                                        ", Endereço: "+dono.getEndereco() +
-                                        ", Email: "+dono.getEmail()
-                        );
+                    System.out.println("O que deseja fazer:");
+                    System.out.println("1 - Inserir proprietário");
+                    System.out.println("2 - Excluir proprietário");
+                    System.out.println("3 - Atualizar proprietário");
+                    System.out.println("4 - Voltar");
+                    opcao2 = sc.nextInt();
+                    switch (opcao2) {
+                        case 1:
+                            Proprietario p = lerDadosProprietario();
+                            proprietarioDAO.inserir(p);
+                            System.out.println("\n== LISTA DE PROPRIETÁRIOS ==");
+                            List<Proprietario> proprietarios = proprietarioDAO.listarTodos();
+                            for (Proprietario dono : proprietarios) {
+                                System.out.println(
+                                        "ID: " + dono.getId() +
+                                                ", Nome: " + dono.getNome() +
+                                                ", CPF: " + dono.getCpf() +
+                                                ", Telefone: " + dono.getTelefone() +
+                                                ", Endereço: " + dono.getEndereco() +
+                                                ", Email: " + dono.getEmail()
+                                );
+                            }
+                            break;
+                        case 2:
+                            //EXCLUIR PROPRIETARIO
+                            System.out.println("Digite o CPF do proprietário:");
+                            String cpf = sc.nextLine();
+                            excluirProprietario(cpf);
+                            proprietarios = proprietarioDAO.listarTodos();
+                            for (Proprietario dono : proprietarios) {
+                                System.out.println(
+                                        "ID: " + dono.getId() +
+                                                ", Nome: " + dono.getNome() +
+                                                ", CPF: " + dono.getCpf() +
+                                                ", Telefone: " + dono.getTelefone() +
+                                                ", Endereço: " + dono.getEndereco() +
+                                                ", Email: " + dono.getEmail()
+                                );
+                            }
+                            break;
+
+                        case 3:
+                            //ATUALIZAR PROPRIETÁRIO
+                            cpf = sc.nextLine();
+                            atualizarProprietario(cpf);
+                            proprietarios = proprietarioDAO.listarTodos();
+                            for (Proprietario dono : proprietarios) {
+                                System.out.println(
+                                        "ID: " + dono.getId() +
+                                                ", Nome: " + dono.getNome() +
+                                                ", CPF: " + dono.getCpf() +
+                                                ", Telefone: " + dono.getTelefone() +
+                                                ", Endereço: " + dono.getEndereco() +
+                                                ", Email: " + dono.getEmail()
+                                );
+                            }
+
                     }
                     break;
 
                 case 2:
-                    Animal a = lerDadosAnimal();
-                    animalDAO.inserir(a);
-                    System.out.println("\n== LISTA DE ANIMAIS ==");
-                    List<Animal> animais = animalDAO.listarTodos();
-                    for(Animal bicho : animais){
-                        System.out.println(
-                                "ID: " + bicho.getId() +
-                                        ", Nome: " + bicho.getNome() +
-                                        ", Espécie: " + bicho.getEspecie() +
-                                        ", Raça: " + bicho.getRaca() +
-                                        ", Nascimento: " + bicho.getData_de_nascimento() +
-                                        ", Peso: " + bicho.getPeso() +
-                                        ", Proprietário ID: " + bicho.getId_proprietario()
-                        );
+                    System.out.println("O que deseja fazer:");
+                    System.out.println("1 - Inserir animal");
+                    System.out.println("2 - Excluir animal");
+                    System.out.println("3 - Atualizar animal");
+                    System.out.println("4 - Voltar");
+                    opcao2 = sc.nextInt();
+                    switch (opcao2) {
+                        case 1:
+                            Animal a = lerDadosAnimal();
+                            animalDAO.inserir(a);
+                            System.out.println("\n== LISTA DE ANIMAIS ==");
+                            List<Animal> animais = animalDAO.listarTodos();
+                            for (Animal bicho : animais) {
+                                System.out.println(
+                                        "ID: " + bicho.getId() +
+                                                ", Nome: " + bicho.getNome() +
+                                                ", Espécie: " + bicho.getEspecie() +
+                                                ", Raça: " + bicho.getRaca() +
+                                                ", Nascimento: " + bicho.getData_de_nascimento() +
+                                                ", Peso: " + bicho.getPeso() +
+                                                ", Proprietário ID: " + bicho.getId_proprietario()
+                                );
+                            }
+                            break;
+
+                        case 2:
+                            //EXCLUIR ANIMAL
+                            System.out.println("Digite o ID do Animal:");
+                            int id = sc.nextInt();
+                            excluirAnimal(id);
+                            System.out.println("\n== LISTA DE ANIMAIS ==");
+                            animais = animalDAO.listarTodos();
+                            for (Animal bicho : animais) {
+                                System.out.println(
+                                        "ID: " + bicho.getId() +
+                                                ", Nome: " + bicho.getNome() +
+                                                ", Espécie: " + bicho.getEspecie() +
+                                                ", Raça: " + bicho.getRaca() +
+                                                ", Nascimento: " + bicho.getData_de_nascimento() +
+                                                ", Peso: " + bicho.getPeso() +
+                                                ", Proprietário ID: " + bicho.getId_proprietario()
+                                );
+                            }
+                            break;
+                        case 3:
+                            //ATUALIZAR ANIMAL
+                            System.out.println("Digite o ID do Animal:");
+                             id = sc.nextInt();
+                             atualizarAnimal(id);
+                            System.out.println("\n== LISTA DE ANIMAIS ==");
+                            animais = animalDAO.listarTodos();
+                            for (Animal bicho : animais) {
+                                System.out.println(
+                                        "ID: " + bicho.getId() +
+                                                ", Nome: " + bicho.getNome() +
+                                                ", Espécie: " + bicho.getEspecie() +
+                                                ", Raça: " + bicho.getRaca() +
+                                                ", Nascimento: " + bicho.getData_de_nascimento() +
+                                                ", Peso: " + bicho.getPeso() +
+                                                ", Proprietário ID: " + bicho.getId_proprietario()
+                                );
+                            }
                     }
                     break;
 
-                // outros menus...
             }
-
         } while (opcao != 0);
 
         System.out.println("Encerrando...");
     }
 }
+
