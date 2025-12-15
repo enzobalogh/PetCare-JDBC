c75265eee07d0910d2d831122cf3311f498e68b6
enzobalogh <enzobalogh2@gmail.com>
2025-12-15 12:54:59 -0300

    Configura autenticação de conexão com PostgreSQL
    Implementa autenticação por senha e validação inicial de conexão PostgreSQL.

--- src/main/java/br/com/petcare/database/ConnectionFactory.java
+++ src/main/java/br/com/petcare/database/ConnectionFactory.java
@@ -2,12 +2,29 @@ package br.com.petcare.database;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.util.Scanner;
+
+import static br.com.petcare.view.Menu.sc;
 
 public class ConnectionFactory {
     private static final String URL = "jdbc:postgresql://localhost:5432/clinicaveterinariadb";
     private static final String USER = "postgres";
-    private static final String PASSWORD = ""; //Meu pgAdmin não tem senha
+    private static String PASSWORD;
 
+    public static void testarConexao(){
+        try (Connection con = getConnection()){
+            System.out.println("Conexão com o banco realizada com sucesso!");
+        } catch (Exception e){
+            throw new RuntimeException("Falha ao conectar no banco: "+e.getMessage());
+        }
+    }
+
+    public static void pedeSenha() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite a senha do seu banco:");
+        PASSWORD = sc.nextLine();
+    }
+    //Meu pgAdmin não tem senha
     public static Connection getConnection(){
         try{
             return DriverManager.getConnection(URL, USER, PASSWORD);
@@ -15,4 +32,5 @@ public class ConnectionFactory {
             throw new RuntimeException("Erro ao conectar ao banco "+ e.getMessage());
         }
     }
+
 }
